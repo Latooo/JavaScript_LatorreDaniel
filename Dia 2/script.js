3ed3fdfa943d6fd9c51f90c336c3b105e2756047
Latooo <daniellatorre600@gmail.com>
2024-04-14 13:11:00 -0500

    feat: :sparkles: Creacion de menu para usuario
    Se creo el menu con las distintas opciones que va a poseer el programa

---
+++ Dia 2/script.js
@@ -0,0 +1,11 @@
+function menuPrincipal() {
+    console.log("\n--- Menú Principal ---");
+    console.log("1. Iniciar Sesión como Coordinador");
+    console.log("2. Iniciar Sesión como Trainer");
+    console.log("3. Iniciar Sesión como Camper");
+    console.log("4. Reportes");
+    console.log("5. Salir del programa");
+}
+
+// Ejemplo de uso
+menuPrincipal();
