cc009bc34b8dbcc3332bb79c2c18763e7c79b0b2
Latooo <daniellatorre600@gmail.com>
2024-04-16 17:56:31 -0500

    feat: :sparkles: Creacion de nueva funcion para ReplaceWords
    Se creo una nueva funcion para el ejercicio de reemplazar palabras

---
+++ Dia 3/Replace Words/script.js
@@ -0,0 +1,12 @@
+var replaceWords = function(dictionary, sentence) {
+    let wordArr = sentence.split(" ");
+    wordArr = wordArr.map(w => {
+        for(let i = 0; i <= dictionary.length - 1; i++) {
+            if (w.indexOf(dictionary[i]) === 0) {
+                w = dictionary[i];
+            }
+        }
+        return w;
+    })
+    return wordArr.reduce((str, word) => str += `${word} `, "").trim();
+};
