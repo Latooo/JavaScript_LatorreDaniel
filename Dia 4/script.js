4fc0ab970f775108003adce69515f90f72b18ab7
Latooo <daniellatorre600@gmail.com>
2024-04-18 06:42:23 -0500

    feat: :sparkles: Se crearon diferentes objetos
    Se crearon diferentes tipos de objetos para POO

---
+++ Dia 4/script.js
@@ -0,0 +1,26 @@
+let persona = {
+    nombre: "Juan",
+    edad: 30,
+    genero: "masculino",
+    ocupacion: "ingeniero"
+  };
+
+  let coche = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2022,
+    color: "rojo"
+  };
+
+  let libro = {
+    titulo: "Cien años de soledad",
+    autor: "Gabriel García Márquez",
+    añoPublicacion: 1967,
+    genero: "realismo mágico"
+  };
+  
+  let numero = 42;
+
+  function suma(a, b) {
+    return a + b;
+  }
